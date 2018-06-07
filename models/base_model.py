247357d25d0458c088a46dc900ff51b8aad38e82
flourishcodes <kunle@adeniran.com>
2018-06-07 15:22:25 -0700

    Python script for BaseModel of HBNB prpoject

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""Class BaseModel"""
+
+
+import uuid
+from datetime import datetime
+import json
+
+
+class BaseModel:
+    """Base Class with public instance attributes and methods"""
+
+
+    def __init__(self, id=None, created_at="", updated_at=""):
+    	"""Instantiates the attributes of the BaseModel class"""
+    	if id is not None:
+    		self.id = id
+    	else:
+    		self.id = str(uuid.uuid4())
+    		self.created_at = datetime.now()
+    		self.updated_at = datetime.now()
+
+
+    def __str__(self):
+    	""" Return the human readable print format"""
+    	a, b = self.id, self.__dict__
+    	return("[BaseModel] ({}) {}".format(a, b))
+
+    def save(self):
+    	"""Shows the newly updated time from time of instance creation"""
+    	self.updated_at = datetime.now()
+
+    def to_dict(self):
+    	"""Returns a dictionay containing all keys/values"""
+    	pass
