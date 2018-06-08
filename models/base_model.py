5bb7104583a281b4b57972883b5221258800b6f6
flourishcodes <kunle@adeniran.com>
2018-06-08 01:29:21 -0700

    Updated Base model

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.save()
 
     def __str__(self):
         """ Return the human readable print format"""
