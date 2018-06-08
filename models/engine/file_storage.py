61b877dbbfd58a681aaa3f3531357683302435f2
flourishcodes <kunle@adeniran.com>
2018-06-08 16:35:35 -0700

    Made changes to save(self) function, corrected typos

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,20 +16,20 @@ class FileStorage:
 
         def new(self, obj):
                 """Returns __objects with obj set as key"""
-                key = '{}.{}'.format( obj.__class__.__name__, id)
-                self.__object[key] = obj
+                key = '{}.{}'.format( obj.__class__.__name__, obj.id)
+                self.__objects[key] = obj
 
         def save(self):
                 """Serializes __objects to JSON file inside"""
                 save_file = self.__file_path
                 new_dict = {}
                 for key, item in self.__objects.items():
-                        if type(obj) is dict:
+                        if type(item) is dict:
                                 new_dic[key] = item
                         else:
-                                new_dict[key] = obj.to_dict()
+                                new_dict[key] = item.to_dict()
                 with open(save_file, "w+", encoding='utf-8') as new_file:
-                        json.dump(new_dict, mew_file)
+                        json.dump(new_dict, new_file)
 
         def reload(self):
                 """Deserializes the JSON file to __objects
