3cb61ea7505340c6ba826a1605cadc36df49516a
vmdn23 <victormdnguyen88@gmail.com>
2018-06-09 21:43:27 +0000

    create a user class

--- models/user.py
+++ models/user.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Users"""
+
+
+import models
+import models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """A user class that inherits from BaseModel"""
+    email = " "
+    password = " "
+    first_name = " "
+    last_name = " "
