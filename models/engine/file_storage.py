2fc54706b0970042b0ca3045ae0dd993f7392baa
vmdn23 <victormdnguyen88@gmail.com>
2018-06-10 03:29:29 +0000

    change new back to self.__objects[key] = obj

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,7 +19,7 @@ class FileStorage:
     def new(self, obj):
         """Returns __objects with obj set as key"""
         key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj.to_dict()
+        self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to JSON file inside"""
